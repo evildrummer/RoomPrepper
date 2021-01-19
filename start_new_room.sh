25ac381bbd4dc3bd99550eaba5220d1536f3f9bc
MightyIT <m_hoffmann83@outlook.de>
2021-01-19 11:36:13 +0100

    Update start_new_room.sh
    - added second try of ip input
    - added folder screenshots

--- start_new_room.sh
+++ start_new_room.sh
@@ -15,47 +15,82 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+pause(){
+
+ read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+
+}
 
 
 # set variables
 read -p 'Which platform are you using (THM or HTB) ?: ' PLATFORM
-read -p 'Type in the IP address for the machine (e.g. 10.11.12.3): ' IP
 read -p 'Enter the name of the machine (e.g. HackPark): ' HOST
 BOXDIR=~/$PLATFORM/$HOST
 
+# check if directory already exists
+if test -d $BOXDIR; then
 
+	echo -e "${RED}Directory already exists !${NC}"
+	exit 0
+fi
 
-# Pause function for debugging
-pause(){
- read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
-}
-
+read -p 'Type in the IP address for the machine (e.g. 10.11.12.3): ' IP
+echo $IP | xclip -selection clipboard
 
 clear
 
-# Ping the IP from arguments to verify VPN/Network connectivity
+# Ping the IP from input to verify VPN/Network connectivity
+
 echo -e  " \n"
 echo -e "${YELLOW}-----------------------------${NC}"
-echo -e "${YELLOW}- Checking Host onnectivity - ${NC}"
+echo -e "${YELLOW}- Checking Host Connectivity - ${NC}"
 echo -e "${YELLOW}-----------------------------${NC}"
 echo -e " \n"
 
-
+echo $IP > ip.txt
 if ping -q -c 1 -W 1 $IP >/dev/null; then
-  echo -e "${GREEN}Host is reachable${NC}"
-else
-  echo -e "${RED}Host is NOT reachable${NC}"
-  echo "Starting VPN Connection first!"
-  exit 0
+
+        echo -e "${GREEN}Host is reachable${NC}"
+        echo -e "${GREEN}Die IP lautet: $IP${NC}"
+
+
+    else
+        echo -e "${RED}Host is NOT reachable${NC}"
+        read -p 'Starting VPN Connection first or type in IP again and hit enter to continue (last value = '$IP') : ' IP2
+
+        if test -z $IP2; then
+
+            echo 'pinging IP -> ' $IP''
+            if ping -q -c 1 -W 1 $IP >/dev/null;then
+            echo -e "${GREEN}Host is reachable${NC}"
+            else
+                echo -e "${RED}Host is NOT reachable! exiting....${NC}"
+                exit 0
+            fi
+
+        else
+            echo 'pinging IP again -> ' $IP2''
+            if ping -q -c 1 -W 1 $IP2 >/dev/null ; then
+                echo -e "${GREEN}Host is reachable${NC}"
+                echo $IP2 > ip.txt
+                IP=$(cat ip.txt)
+
+            else
+                echo -e "${RED}Host is NOT reachable! exiting....${NC}"
+                exit 0
+            fi
+        fi
 fi
 
-sleep 2
+
+sleep 1
 
 # Creating the folders
 # ~/PLATFORM/
 # ~/PLATFORM/HOSTNAME
 # ~/PLATFORM/HOSTNAME/nmap
-
+# ~/PLATFORM/HOSTNAME/log
+# ~/PLATFORM/HOSTNAME/screenshots
 
 echo -e "\n"
 echo -e "${YELLOW}------------------------${NC}"
@@ -67,7 +102,13 @@ mkdir $BOXDIR
 echo "--> Folder for room $HOST created ..."
 sleep 1
 mkdir $BOXDIR/nmap
-echo "--> nmap Folder created ..."
+echo "--> nmap folder created ..."
+sleep 1
+mkdir $BOXDIR/log
+echo "--> log folder created ..."
+mkdir $BOXDIR/screenshots
+echo "--> screenshots folder created ..."
+
 
 sleep 1
 
@@ -78,9 +119,9 @@ echo -e "${YELLOW}-  Starting standard Nmap Scan -${NC}"
 echo -e "${YELLOW}--------------------------------${NC}"
 echo -e "\n"
 
-sleep 3
+sleep 2
 
-nmap -sCV $IP -v -T4 -oA $BOXDIR/nmap/initial
+nmap -sCV $IP -v -T5  -oA $BOXDIR/nmap/initial
 
 
 echo -e "\n"
@@ -104,27 +145,23 @@ echo -e "$HOST \n" >> $BOXDIR/notes.md
 
 sleep 1
 
-echo "--> Fill open ports section"
-echo -e "[open ports]" >> $BOXDIR/notes.md
-
-sleep 1
-
 echo "--> import remaining template and files"
 cat fillin.txt >> $BOXDIR/notes.md
 cat notes_template.md >> $BOXDIR/notes.md
-cp createList.sh $BOXDIR
-chmod +x $BOXDIR/createList.sh
+cp createLists.sh $BOXDIR
+chmod +x $BOXDIR/createLists.sh
+
 
 sleep 1
 
 echo "--> Cleaning files"
-rm fillin.txt
+rm fillin.txt ip.txt
 
-sleep 3
+sleep 2
 
 echo -e "\n"
 echo -e "${GREEN}FINISHED! Opening notes${NC}"
 
-sleep 5
+sleep 4
 
 nano $BOXDIR/notes.md
